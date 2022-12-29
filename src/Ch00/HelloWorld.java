36f7ff876a04ecb82b216bf4837d146b0199df03
LeeSumin <lee5911556@hanmail.net>
2022-12-29 17:35:26 +0900

    Ch02

---
+++ src/Ch00/HelloWorld.java
@@ -0,0 +1,12 @@
+package Ch00; // 패키지명(폴더와 유사)
+
+public class HelloWorld    // 클래스 영역 - 객체 지향 문법 적용 단위
+{
+	// 메서드(방법, 기능) : 작업수행을 하는데 필요한 여러코드를
+	// main 메서드 :
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
